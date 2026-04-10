a5ab7ed016471681431982d45bc38e519dfc9fa0
Adepu Sri Charan <sricharan.adepu36@gmail.com>
2026-04-10 15:53:58 +0530

    feat(lc): problem 16

---
+++ src/leetcode/threeSum_15/ThreeSumTwoPointerSet.java
@@ -0,0 +1,34 @@
+package leetcode.threeSum_15;
+
+import java.util.*;
+
+public class ThreeSumTwoPointerSet {
+    private static List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return List.copyOf(result);
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> res = threeSum(nums);
+        System.out.println(res);
+    }
+}
