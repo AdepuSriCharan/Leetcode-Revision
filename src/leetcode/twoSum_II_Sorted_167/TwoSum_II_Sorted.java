7e4a3fc8aa6ddd4618fe1e66c18b6284ff5d1693
Adepu Sri Charan <sricharan.adepu36@gmail.com>
2026-04-15 22:32:54 +0530

    feat(lc): revise problem 167

---
+++ src/leetcode/twoSum_II_Sorted_167/TwoSum_II_Sorted.java
@@ -0,0 +1,29 @@
+package leetcode.twoSum_II_Sorted_167;
+
+import java.util.Arrays;
+
+public class TwoSum_II_Sorted {
+    private static int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{-1, -1};
+    }
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: " + Arrays.toString(result));
+    }
+}
