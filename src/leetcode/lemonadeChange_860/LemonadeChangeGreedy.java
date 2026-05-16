5d7dfda52f41482a66c06eb7723dccf434f47839
Adepu Sri Charan <sricharan.adepu36@gmail.com>
2026-05-16 19:42:18 +0530

    feat(lc): problem 860

---
+++ src/leetcode/lemonadeChange_860/LemonadeChangeGreedy.java
@@ -0,0 +1,36 @@
+package leetcode.lemonadeChange_860;
+
+public class LemonadeChangeGreedy {
+    private static boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+
+        for(int bill : bills){
+            if(bill == 5){
+                five++;
+            } else if(bill == 10){
+                if(five > 0){
+                    five--;
+                    ten++;
+                } else{
+                    return false;
+                }
+            } else{
+                if(ten > 0 && five > 0){
+                    ten--;
+                    five--;
+                } else if(five >= 3){
+                    five -= 3;
+                } else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+
+        int[] bills = {5,5,5,10,20};
+
+        System.out.println(lemonadeChange(bills));
+    }
+}
